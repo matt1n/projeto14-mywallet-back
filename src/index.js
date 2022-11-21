a2d0703b5cfa4ff813b3099cea533212557c4c70
Mateus Macêdo <mateusamacedo5@gmail.com>
2022-11-21 16:48:58 -0300

    alter port

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import authRouters from "./routes/authRoutes.js"
 import walletRouters from "./routes/walletRoutes.js"
+import dotenv from "dotenv";
 
 const app = express();
 app.use(express.json());
@@ -9,4 +10,6 @@ app.use(cors());
 app.use(authRouters)
 app.use(walletRouters)
 
-app.listen(5000, () => console.log("Server running in port: 5000"));
+const port = process.env.PORT || 5000
+
+app.listen(port, () => console.log(`Server running in port: ${port}`));
